55ae5aa6b4fb35f3df821bd3ede054f40d752e90
7Bpencil <Edward.Ekb@yandex.ru>
2026-09-10 02:58:31 +0500

    basic setup

--- Class1.cs
+++ Class1.cs
@@ -1,7 +1,13 @@
-﻿namespace Test
-{
-    public class Class1
-    {
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
 
+namespace SevenBoldPencil.Test
+{
+    [BepInPlugin("7Bpencil.Test", "7Bpencil.Test", "0.0.1")]
+    public class Plugin : BasePlugin
+    {
+    	public override void Load()
+    	{
+    	}
     }
 }
