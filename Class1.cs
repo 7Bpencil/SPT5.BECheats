f7e2e8939224a670ff4b6285f7d2817534fb1910
7Bpencil <Edward.Ekb@yandex.ru>
2026-09-10 03:36:32 +0500

    yes

--- Class1.cs
+++ Class1.cs
@@ -1,13 +1,67 @@
 ﻿using BepInEx;
 using BepInEx.Unity.IL2CPP;
+using Comfort.Common;
+using EFT;
+using EFT.HealthSystem;
+using EFT.InventoryLogic;
+using HarmonyLib;
+using SPTushonka.Reflection.Patching;
+using System.Reflection;
 
-namespace SevenBoldPencil.Test
+namespace SevenBoldPencil.Test;
+
+[BepInPlugin("7Bpencil.Test", "7Bpencil.Test", "0.0.1")]
+public class Plugin : BasePlugin
 {
-    [BepInPlugin("7Bpencil.Test", "7Bpencil.Test", "0.0.1")]
-    public class Plugin : BasePlugin
+	public override void Load()
+	{
+        new Patch_ActiveHealthController_ApplyDamage().Enable();
+        new Patch_Physical_Update().Enable();
+	}
+}
+
+public class Patch_ActiveHealthController_ApplyDamage : ModulePatch
+{
+	protected override MethodBase GetTargetMethod()
+	{
+		return AccessTools.Method(typeof(ActiveHealthController), nameof(ActiveHealthController.ApplyDamage));
+	}
+
+    [PatchPrefix]
+    public static bool Prefix(ActiveHealthController __instance, ref float __result)
     {
-    	public override void Load()
-    	{
-    	}
+        if (!Singleton<GameWorld>.Instantiated)
+        {
+            return true;
+        }
+
+        var mainPlayer = Singleton<GameWorld>.Instance.MainPlayer;
+        if (mainPlayer && mainPlayer.ActiveHealthController == __instance)
+        {
+            __result = 0f;
+            return false;
+        }
+
+        return true;
+    }
+}
+
+public class Patch_Physical_Update : ModulePatch
+{
+	protected override MethodBase GetTargetMethod()
+	{
+		return AccessTools.Method(typeof(Physical), nameof(Physical.Update));
+	}
+
+    [PatchPostfix]
+    public static void Postfix(Physical __instance)
+    {
+        var player = __instance._player;
+        if (player && player.IsYourPlayer)
+        {
+            __instance.Stamina.Current = __instance.Stamina.TotalCapacity;
+            __instance.HandsStamina.Current = __instance.HandsStamina.TotalCapacity;
+            __instance.Oxygen.Current = __instance.Oxygen.TotalCapacity;
+        }
     }
 }
